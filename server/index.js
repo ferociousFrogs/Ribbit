7fed408c392813ae050fe6cbbc88d02d23172ffa
dshelters <dshelters@gmail.com>
2017-04-07 16:33:40 -0700

    (implement) Set up server + minor edits
    Make react dependancies dev only since they will be transpiled.
    Serve up index.html on '/' get request.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
 // express server
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+// Middleware
+app.use(bodyParser.json());
+app.use('/', express.static(path.join(__dirname, '../client')));
+
+
+// Routes
+app.get('/', (req, res) => {
+  res.send();
+}); 
+
+
+app.listen(3000, function () {
+  console.log('Ribbit app listening on port 3000!')
+});
