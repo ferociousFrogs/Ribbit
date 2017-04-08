189ce77edb836261656cea455d93424db82f3798
jefQuery <jeffrey.milberger@gmail.com>
2017-04-08 16:53:20 -0700

    add snapshot test

--- client/render.jsx
+++ client/render.jsx
@@ -3,6 +3,4 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 
-
 render(<Router><App /></Router>, document.getElementById('app'));
-
