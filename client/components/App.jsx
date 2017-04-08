876dcabe9116632be9e81e0302671a21fa274cd0
Andy Yeo <andy.yeo@gmail.com>
2017-04-08 13:55:15 -0700

    Fix webpack config file

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './homepage/Home';
 import Room from './room/Room';
 
 const App = () => (
-  <div>
-    <p>Hello David!</p>
-    <Navbar />
-    <Home />
-    <Room />
-  </div>
+  <Router>
+    <div>
+      <p>Hello David!</p>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route path="/room" component={Room} />
+    </div>
+  </Router>
 );
 
 render(<App />, document.getElementById('app'));
