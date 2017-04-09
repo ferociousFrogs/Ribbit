7ead88e84f8596b01af9a2575f977b427cbcfea6
jefQuery <jeffrey.milberger@gmail.com>
2017-04-08 17:19:06 -0700

    Add MVP coding workspace

--- client/components/room/Workspace.jsx
+++ client/components/room/Workspace.jsx
@@ -7,9 +7,9 @@ class Workspace extends React.Component {
 
   render() {
     return (
-        <div className="row border left-side">
-          This is the workspace component
-        </div>
+      <div className="row border left-side">
+        <iframe frameBorder="0" width="100%" height="100%" src="https://repl.it/languages" />
+      </div>
     );
   }
 }
