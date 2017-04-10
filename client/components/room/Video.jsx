c52dd612d9cff3c8ad0e0544d484c6e7805e86cf
Andy Yeo <andy.yeo@gmail.com>
2017-04-10 15:37:53 -0700

    Create initial local video interface with webRTC

--- client/components/room/Video.jsx
+++ client/components/room/Video.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
 
+const constraints = {
+  audio: true,
+  video: true
+};
+
+// const navigator.getUserMedia = navigator.getUserMedia ||
+//       navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
 class Video extends React.Component {
   constructor(props) {
     super(props);
+    this.successCallback = this.successCallback.bind(this);
+    this.errorCallback = this.errorCallback.bind(this);
+    this.triggerGetUserMedia = this.triggerGetUserMedia.bind(this);
+  }
+
+  componentDidMount() {
+    this.triggerGetUserMedia();
+  }
+
+  successCallback(stream) {
+    window.stream = stream; // stream available to console
+    if (window.URL) {
+      this.refs.video.src = window.URL.createObjectURL(stream);
+    } else {
+      this.refs.video.src = stream;
+    }
+  }
+
+  errorCallback(error) {
+    console.log('navigator.getUserMedia error: ', error);
+  }
+
+  triggerGetUserMedia() {
+    navigator.getUserMedia(constraints, this.successCallback, this.errorCallback);
   }
 
   render() {
     return (
       <div className="row border right-side">
-        This is the video component
+        <video ref="video" autoPlay />
+
       </div>
     );
   }
