cf95f416eb86e249b525e86065bca0c218699d35
David Brodie <david.brodie122@gmail.com>
2017-04-10 12:29:16 -0700

    Make basic styling for chat component. No functionality yet

--- client/components/room/Chat.jsx
+++ client/components/room/Chat.jsx
@@ -8,7 +8,23 @@ class Chat extends React.Component {
   render() {
     return (
       <div className="row border right-side">
-        This is the chat component
+        <div className="chat-header">
+          <span className="glyphicon glyphicon-user" />
+          <span>Chat with USER</span>
+        </div>
+        <div className="chat-area">
+          chat area
+        </div>
+        <div className="chat-input">
+          <form>
+            <input
+              type="text"
+              className="col-md-10 col-sm-10 col-xs-10"
+              placeholder="Type your message here."
+            />
+            <button className="btn-info col-md-2  col-sm-2 col-xs-2">Send</button>
+          </form>
+        </div>
       </div>
     );
   }
