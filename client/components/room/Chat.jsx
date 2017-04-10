464b76368b8071c2abac0d7b9d7ac9304e71ea75
David Brodie <david.brodie122@gmail.com>
2017-04-10 15:35:25 -0700

    Add basic classes to styles.css

--- client/components/room/Chat.jsx
+++ client/components/room/Chat.jsx
@@ -3,27 +3,35 @@ import React from 'react';
 class Chat extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {};
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
   }
 
   render() {
     return (
       <div className="row border right-side">
-        <div className="chat-header">
-          <span className="glyphicon glyphicon-user" />
-          <span>Chat with USER</span>
-        </div>
-        <div className="chat-area">
-          chat area
-        </div>
-        <div className="chat-input">
-          <form>
-            <input
-              type="text"
-              className="col-md-10 col-sm-10 col-xs-10"
-              placeholder="Type your message here."
-            />
-            <button className="btn-info col-md-2  col-sm-2 col-xs-2">Send</button>
-          </form>
+        <div className="container-fluid chat-container">
+          <div className="chat-header">
+            <span className="glyphicon glyphicon-user" />
+            <span>    Chat with USER</span>
+          </div>
+          <div className="chat-area">
+            chat area
+          </div>
+          <div className="chat-input">
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                className="col-md-10 col-sm-10 col-xs-10"
+                placeholder="Type your message here."
+              />
+              <button className="btn-info col-md-2  col-sm-2 col-xs-2">Send</button>
+            </form>
+          </div>
         </div>
       </div>
     );
