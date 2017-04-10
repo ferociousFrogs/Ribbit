aa4119ddc6d6cdd9df9e2cfbf79aa44d267f532e
David Brodie <david.brodie122@gmail.com>
2017-04-10 11:18:57 -0700

    Add enzyme and react test utils as dependencies

--- spec/client-spec/Chat.test.jsx
+++ spec/client-spec/Chat.test.jsx
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 import { renderIntoDocument } from 'react-dom/test-utils';
+=======
+>>>>>>> Add enzyme and react test utils as dependencies
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Chat from '../../client/components/room/Chat';
@@ -8,7 +11,10 @@ test('Chat component snapshot test', () => {
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
+<<<<<<< HEAD
 
 test('Chat component should be a stateful class component', () => {
   expect(React.Component.isPrototypeOf(Chat)).toEqual(true);
 });
+=======
+>>>>>>> Add enzyme and react test utils as dependencies
