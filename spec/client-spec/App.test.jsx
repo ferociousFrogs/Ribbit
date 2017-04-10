4af4eb23d376d1d4f12ed1814d0df8b7a6bd219d
David Brodie <david.brodie122@gmail.com>
2017-04-10 10:32:52 -0700

    Update folder tree for spec. Update test message for index.js

--- spec/client-spec/App.test.jsx
+++ spec/client-spec/App.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from '../components/App';
+import App from '../../client/components/App';
 
 test('App component snapshot test', () => {
   const component = renderer.create(<Router><App /></Router>);
