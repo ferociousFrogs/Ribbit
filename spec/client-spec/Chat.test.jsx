12f3de3555606ce029af2caa79dda06f2500abb7
David Brodie <david.brodie122@gmail.com>
2017-04-10 11:46:05 -0700

    Update dependencies to remove warning. Add test to Chat.test.jsx

--- spec/client-spec/Chat.test.jsx
+++ spec/client-spec/Chat.test.jsx
@@ -1,3 +1,4 @@
+import { renderIntoDocument } from 'react-dom/test-utils';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Chat from '../../client/components/room/Chat';
@@ -6,4 +7,8 @@ test('Chat component snapshot test', () => {
   const component = renderer.create(<Chat />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test('Chat component should be a stateful class component', () => {
+  expect(React.Component.isPrototypeOf(Chat)).toEqual(true);
 });
