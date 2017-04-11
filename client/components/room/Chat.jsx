a37750bd9ec763c640cb51d0142985a54fa12584
David Brodie <david.brodie122@gmail.com>
2017-04-11 14:08:53 -0700

    Make chat work for appending text to page (no sockets, saved state)

--- client/components/room/Chat.jsx
+++ client/components/room/Chat.jsx
@@ -7,12 +7,25 @@ let socket = io('http://localhost:3000');
 class Chat extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      text: '',
+      user: [{ name: 'Batman' }, { name: 'Bane' }],
+      messages: []
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleMessage = this.handleMessage.bind(this);
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
+  handleMessage(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const prevMessages = this.state.messages;
+    prevMessages.push(this.state.text);
+    this.setState({ messages: prevMessages });
+    this.setState({ text: '' });
   }
 
   render() {
@@ -23,13 +36,15 @@ class Chat extends React.Component {
             <span className="glyphicon glyphicon-user" />
             <span>    Chat with USER</span>
           </div>
-          <ChatWindow />
+          <ChatWindow messages={this.state.messages} />
           <div className="chat-input">
             <form onSubmit={this.handleSubmit}>
               <input
                 type="text"
                 className="col-md-10 col-sm-10 col-xs-10"
                 placeholder="Type your message here."
+                value={this.state.text}
+                onChange={this.handleMessage}
               />
               <button className="btn-info col-md-2  col-sm-2 col-xs-2">Send</button>
             </form>
@@ -41,3 +56,11 @@ class Chat extends React.Component {
 }
 
 export default Chat;
+
+
+// [] Get data to append to the chat window on submit
+// [] set up function to grab data from the input box and update state
+// [] pass that data down to be rendered by the chatwindow and chatmessage components
+// [] for now, store messages in an array and map them all
+// []
+// []
