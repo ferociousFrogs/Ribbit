b865dd41ecf7438b663321293e6c82d69f8563e1
David Brodie <david.brodie122@gmail.com>
2017-04-10 17:04:10 -0700

    Verify connection of sockets to components

--- server/index.js
+++ server/index.js
@@ -20,6 +20,9 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 http.listen(port, () => {
