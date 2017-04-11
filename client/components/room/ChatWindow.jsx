d4c92e64c1c08422b93c51b0f231267d91920af5
David Brodie <david.brodie122@gmail.com>
2017-04-10 19:55:24 -0700

    Add ChatWindow/ChatMessage components

---
+++ client/components/room/ChatWindow.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ChatMessage from './ChatMessage';
+
+const ChatWindow = (props) => (
+  <div className="chat-area">
+    <ChatMessage />
+  </div>
+);
+
+export default ChatWindow;
