df800dd20d02a071a093a5fa399e8564407fdb77
Andy Yeo <andy.yeo@gmail.com>
2017-04-11 16:22:46 -0700

    Implement video and audio toggle button functionality

--- client/components/room/Video.jsx
+++ client/components/room/Video.jsx
@@ -1,30 +1,52 @@
 import React from 'react';
 import webrtc from 'webrtc-adapter';
 
+let localStream;
+let pc1;
+let pc2;
+let offerOptions = {
+  offerToReceiveAudio: 1,
+  offerToReceiveVideo: 1
+};
+
 class Video extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      videoSrc: ''
+      constraints: {
+        video: true,
+        audio: true
+      },
+      video: 'Video Off',
+      mute: 'Mute',
+      localVideoSrc: '',
+      videoScreen: true,
+      startButton: false
     };
-    this.successCallback = this.successCallback.bind(this);
+    this.gotStream = this.gotStream.bind(this);
     this.errorCallback = this.errorCallback.bind(this);
     this.triggerGetUserMedia = this.triggerGetUserMedia.bind(this);
+    this.handleVideoScreen = this.handleVideoScreen.bind(this);
+    this.handleAudio = this.handleAudio.bind(this);
   }
 
   componentDidMount() {
     this.triggerGetUserMedia();
   }
 
-  successCallback(stream) {
-    window.stream = stream;
+  // componentDidUpdate() {
+  //   this.triggerGetUserMedia();
+  // }
+
+  gotStream(stream) {
+    window.localStream = localStream = stream;
     if (window.URL) {
       this.setState({
-        videoSrc: window.URL.createObjectURL(stream)
+        localVideoSrc: window.URL.createObjectURL(stream)
       });
     } else {
       this.setState({
-        videoSrc: stream
+        localVideoSrc: stream
       });
     }
   }
@@ -37,24 +59,38 @@ class Video extends React.Component {
     navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia ||
       navigator.mediaDevices.webkitGetUserMedia || navigator.mediaDevices.mozGetUserMedia;
 
-    const constraints = {
-      audio: true,
-      video: true
-    };
-    return navigator.mediaDevices.getUserMedia(constraints)
-      .then(this.successCallback)
+    return navigator.mediaDevices.getUserMedia(this.state.constraints)
+      .then(this.gotStream)
       .catch(this.errorCallback);
+  }
+
+  handleVideoScreen() {
+    this.setState({
+      constraints: {
+        video: !this.state.constraints.video,
+        audio: this.state.constraints.audio
+      },
+      video: !this.state.constraints.video ? 'Video Off' : 'Video On'
+    }, () => (this.triggerGetUserMedia()));
+  }
+
+  handleAudio() {
+    this.setState({
+      constraints: {
+        video: this.state.constraints.video,
+        audio: !this.state.constraints.audio
+      },
+      mute: !this.state.constraints.audio ? 'Mute' : 'Unmute'
+    }, () => (this.triggerGetUserMedia()));
   }
 
   render() {
     return (
       <div className="row border right-side">
-        <video className="video" src={this.state.videoSrc} autoPlay />
+        <video className="video" src={this.state.localVideoSrc} autoPlay />
         <div>
-          <button id="startButton">Start</button>
-          <button id="callButton">Call</button>
-          <button id="videoOff">Video Off</button>
-          <button id="mute">Mute</button>
+          <button id="videoOff" onClick={this.handleVideoScreen}>{this.state.video}</button>
+          <button id="mute" onClick={this.handleAudio}>{this.state.mute}</button>
         </div>
       </div>
     );
