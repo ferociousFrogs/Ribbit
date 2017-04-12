75b66337d9ef26ff79a685d04f9c94151da86b2e
David Brodie <david.brodie122@gmail.com>
2017-04-12 10:40:39 -0700

    Add socket test to chat.test.jsx (not working)

--- spec/client-spec/Chat.test.jsx
+++ spec/client-spec/Chat.test.jsx
@@ -2,6 +2,7 @@ import { renderIntoDocument } from 'react-dom/test-utils';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Chat from '../../client/components/room/Chat';
+import io from 'socket.io-client';
 
 test('Chat component snapshot test', () => {
   const component = renderer.create(<Chat />);
@@ -12,3 +13,14 @@ test('Chat component snapshot test', () => {
 test('Chat component should be a stateful class component', () => {
   expect(React.Component.isPrototypeOf(Chat)).toEqual(true);
 });
+
+// test('Chat should emit events and pick up responses via listeners', () => {
+//   const port = process.env.PORT || 3000;
+//   const server = `http://localhost:${port}`;
+//   const socket = io(server);
+//   // const updateResponse = message => message;
+//   // const final = updateResponse();
+//   socket.emit('chat message', 'Aww hell no, wut up, dawg?');
+//   socket.on('chat message', );
+//   expect('hello').toEqual('Aww hell no, wut up, dawg?');
+// });
