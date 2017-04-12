56d11f014591b58bc9f9a87ef959eba5e02eda88
jefQuery <jeffrey.milberger@gmail.com>
2017-04-11 17:42:57 -0700

    (feat) replaced iframe with rich text editor

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 
 const BUILD_DIR = path.resolve(__dirname, 'client/public');
@@ -10,19 +11,38 @@ const config = {
     path: BUILD_DIR,
     filename: 'bundle.js'
   },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
+        test: /(\.js|\.jsx)$/,
 
-        include: APP_DIR,
+        include: [
+          APP_DIR,
+          path.join(__dirname, 'node_modules/react-codemirror')
+        ],
         loader: 'babel-loader'
+      },
+      {
+        test: /(\.scss|\.css)$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
       }
     ]
-  }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.scss', '.css'],
+    moduleExtensions: [
+      'node_modules',
+      path.resolve(__dirname, 'src/scripts'),
+      path.resolve(__dirname, 'src')
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css')
+    // this may be pointing at the wrong file endpoint
+  ]
 };
 
 module.exports = config;
