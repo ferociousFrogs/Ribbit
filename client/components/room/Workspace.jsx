a2eea29dc0efe2895358db8a3f00138d6658ecdd
jefQuery <jeffrey.milberger@gmail.com>
2017-04-11 17:42:57 -0700

    (feat) replaced iframe with rich text editor

--- client/components/room/Workspace.jsx
+++ client/components/room/Workspace.jsx
@@ -1,14 +1,46 @@
 import React from 'react';
+import CodeMirror from 'react-codemirror';
+
+require('codemirror/lib/codemirror.css');
+require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/xml/xml');
+require('codemirror/mode/markdown/markdown');
+require('codemirror/mode/python/python');
+require('codemirror/mode/ruby/ruby');
 
 class Workspace extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      code: '// Ribbit'
+    };
+  }
+
+  getInitialState() {
+    return {
+      code: '// Code'
+    };
+  }
+
+  updateCode(newCode) {
+    this.setState({
+      code: newCode
+    });
   }
 
   render() {
+    const options = {
+      lineNumbers: true,
+      mode: 'javascript'
+    };
     return (
       <div className="row border left-side">
-        <iframe frameBorder="0" width="100%" height="100%" src="https://repl.it/languages" />
+        <CodeMirror
+          value={this.state.code}
+          onChange={this.updateCode}
+          options={options}
+        />
+        This is the Workspace Component
       </div>
     );
   }
