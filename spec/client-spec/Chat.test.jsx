b1e54634372641402269d2d7db19a50ab364d72b
David Brodie <david.brodie122@gmail.com>
2017-04-14 11:48:40 -0700

    Fix chat test to work with redux

--- spec/client-spec/Chat.test.jsx
+++ spec/client-spec/Chat.test.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Chat from '../../client/components/room/Chat';
+import store from '../../client/store';
 
 // test('Chat component snapshot test', () => {
 //   const component = renderer.create(<Chat />);
 //   const tree = component.toJSON();
 //   expect(tree).toMatchSnapshot();
 // });
-
 
 test('Chat component should be a stateful class component', () => {
   expect(React.Component.isPrototypeOf(Chat)).toEqual(true);
