a2ccccbefbdb2eac8fe60278b10bdb3cb9528061
dshelters <dshelters@gmail.com>
2017-04-14 12:09:46 -0700

    (feat) Link to user inputted room name in CreateRoute.jsx

--- client/components/homepage/CreateRoute.jsx
+++ client/components/homepage/CreateRoute.jsx
@@ -4,12 +4,29 @@ import { Link } from 'react-router-dom';
 class CreateRoute extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: ''
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    console.log(e.target.value);
+    this.setState({
+      name: e.target.value
+    });
   }
 
   render() {
     return (
       <div className="border">
-        <Link to="/Room">This is our room creation div</Link>
+        <input
+          type="text"
+          value={this.state.name}
+          placeholder="Enter a Room Name"
+          onChange={this.handleNameChange}
+        />
+        <Link to={`/:${this.state.name}`}>This is our room creation div</Link>
       </div>
     );
   }
