79af5f299be2478593791ad134c515b04e8238ba
David Brodie <david.brodie122@gmail.com>
2017-04-15 10:58:17 -0700

    Begin conditional render of chats per unique user

--- client/components/room/ChatMessage.jsx
+++ client/components/room/ChatMessage.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 const ChatMessage = props => (
   <div>
-    <strong>{props.message.userName}</strong>
-    <span className={"name-msg-offset " + (props.message.fromMe ? 'from-me' : '')}>{props.message.text}</span>
+    { props.message.fromMe ?
+      <div>
+        <strong>{props.message.userName}</strong>
+        <span className="name-msg-offset">{props.message.text}</span>
+      </div>
+    :
+      <div>
+        <span className="name-msg-offset">{props.message.text}</span>
+        <strong>{props.message.userName}</strong>
+      </div>
+    }
   </div>
 );
 
 export default ChatMessage;
+
+
+     // <span className={"name-msg-offset " + (props.message.fromMe ? 'from-me' : '')}>{props.message.text}</span>
