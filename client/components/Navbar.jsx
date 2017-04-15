b829080f099b74d540fac0fc586857eacec4af35
David Brodie <david.brodie122@gmail.com>
2017-04-15 16:08:40 -0700

    Update homepage with signup/login + bootstrapping to fit

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <div className="container-fluid border">
-    <Link to="/"><h1>Ribbit</h1></Link>
+    <div className="row">
+      <Link to="/"><h1 className="col-md-10 col-sm-8 col-xs-6">Ribbit</h1></Link>
+      <h2 className="col-md-2 col-sm-4 col-xs-6">signup/login</h2>
+    </div>
   </div>
 );
 
