fac4d8c48ca34005f079a8f3d19169074f4369b7
Andy Yeo <andy.yeo@gmail.com>
2017-04-16 17:38:09 -0700

    Style navigation bar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <div className="container-fluid border">
-    <div className="row">
-      <Link to="/"><h1 className="col-md-10 col-sm-8 col-xs-6">Ribbit</h1></Link>
-      <Link to="/login"><h2 className="col-md-2 col-sm-4 col-xs-6">signup/login</h2></Link>
-    </div>
+  <div className="container-fluid">
+    <header className="header">
+      <div role="navigation" className="navbar navbar-default navbar-fixed-top">
+        <div className="container">
+          <div className="navbar-header"><Link to="/" className="navbar-brand scroll-to"><img src="../ribbit.png" alt="logo" className="hidden-xs hidden-sm" /><span className="sr-only">Go to homepage</span></Link>
+            <div className="navbar-buttons">
+              <button type="button" data-toggle="collapse" data-target=".navbar-collapse" className="navbar-toggle navbar-btn">Menu<i className="pe-7s-menu" /></button>
+            </div>
+          </div>
+          <div id="navigation" className="collapse navbar-collapse navbar-right">
+            <ul className="nav navbar-nav">
+              <li><a href="#features" className="scroll-to">Features</a></li>
+              <li><a href="#integrations" className="scroll-to">Integrations</a></li>
+              <li><a href="#testimonials" className="scroll-to">Testimonials</a></li>
+              <li><a href="#contact" className="scroll-to">Contact</a></li>
+            </ul><Link to="/login" data-toggle="modal" data-target="#get-started" className="btn navbar-btn btn-ghost">Sign Up</Link>
+            <Link to="/login" data-toggle="modal" data-target="#get-started" className="btn navbar-btn btn-ghost">Login</Link>
+          </div>
+        </div>
+      </div>
+    </header>
   </div>
 );
 
