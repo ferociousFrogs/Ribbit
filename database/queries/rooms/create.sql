4eefa121d4909a9f728bb777d54f651d55f65dd4
dshelters <dshelters@gmail.com>
2017-04-18 15:37:25 -0700

    (implement) Begin to set up file structure for db

---
+++ database/queries/rooms/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE rooms
+(
+  id serial PRIMARY KEY,
+  name varchar(40) NOT NULL,
+  owner_one_id INT NOT NULL,
+  owner_two_id INT NOT NULL
+);
--- database/sql/rooms/create.js
+++
@@ -1,5 +0,0 @@
-CREATE TABLE ${schema~}.Room
-(
-    id serial PRIMARY KEY,
-    name text NOT NULL
-);
