8a6e515344d3173c900f560ae85f8e04320d7263
Andy Yeo <andy.yeo@gmail.com>
2017-04-20 15:52:02 -0700

    Add initial code for Facebook OAuth login button

---
+++ client/components/homepage/Facebook.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+class Facebook extends React.Component {
+  constructor(props) {
+    super(props);
+    this.checkLoginState = this.checkLoginState.bind(this);
+    this.testAPI = this.testAPI.bind(this);
+    this.statusChangeCallback = this.statusChangeCallback.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    window.fbAsyncInit = () => {
+      FB.init({
+        appId      : '1871157256463548',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v2.8'
+      });
+      console.log('What is this', FB);
+      this.checkLoginState;
+    };
+  }
+
+
+  checkLoginState() {
+    FB.getLoginStatus((response) => {
+      this.statusChangeCallback(response);
+    });
+  }
+
+  testAPI() {
+    console.log('Welcome! Fetching your information...');
+    FB.api('/me', (response) => {
+      console.log(`Successful login for: ${response.name}`);
+      document.getElementById('status').innerHTML =
+        `Thanks for logging in, ${response.name} !`;
+    });
+  }
+
+  statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    if (response.status === 'connected') {
+      this.testAPI();
+    } else {
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    }
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    window.fbAsyncInit = () => {
+      FB.init({
+        appId      : '1871157256463548',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v2.8'
+      });
+      FB.login(this.statusChangeCallback.bind(this), { scope: 'public_profile, email' });
+    };
+  }
+
+
+  render() {
+    return (
+      <div>
+      <div id="status"></div>
+      <button onClick={this.handleClick.bind(this)}>Login with Facebook</button>
+      </div>
+    );
+  }
+}
+
+export default Facebook;
