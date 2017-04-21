59300c3ecad0146e521e6ec93e927b152436e7ac
Andy Yeo <andy.yeo@gmail.com>
2017-04-21 12:28:46 -0700

    Update ID credentials

--- server/initPassport.js
+++ server/initPassport.js
@@ -1,10 +1,10 @@
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
-const dotenv = require('dotenv');
+require('dotenv').config();
 
 passport.use(new FacebookStrategy({
     clientID: '1871157256463548' || process.env.FB_APPID,
-    clientSecret: '52835e89abe4d1b8386007a918f0a37c' || process.env.FB_APPSECRET
+    clientSecret: process.env.FB_APPSECRET
     // callbackURL: "http://www.example.com/auth/facebook/callback"
 },
   (accessToken, refreshToken, profile, done) => {
@@ -15,7 +15,6 @@ passport.use(new FacebookStrategy({
     });
   }
 ));
-
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
