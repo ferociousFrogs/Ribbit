5e0a7f7d53c3ab3d04c5dc64afa582c132ad7cfe
Andy Yeo <andy.yeo@gmail.com>
2017-04-22 15:11:24 -0700

    Add customers component

---
+++ client/components/homepage/Customers.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Customers = () => (
+  <section id="customers" className="section background-gray-lightest padding--small">
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-4 col-md-2 col-xs-6">
+          <div className="customer"><img src="../img/hackreactor.png" title="Hack Reactor" alt="" className="img-responsive" /></div>
+        </div>
+        <div className="col-sm-4 col-md-2 col-xs-6">
+          <div className="customer"><img src="../img/telegraph-academy.png" title="Telegraph Academy" alt="" className="img-responsive" /></div>
+        </div>
+        <div className="col-sm-4 col-md-2 col-xs-6">
+          <div className="customer"><img src="../img/google.png" title="Google" alt="" className="img-responsive" /></div>
+        </div>
+        <div className="col-sm-4 col-md-2 col-xs-6">
+          <div className="customer"><img src="../img/amazon.png" title="Amazon" alt="" className="img-responsive" /></div>
+        </div>
+        <div className="col-sm-4 col-md-2 col-xs-6">
+          <div className="customer"><img src="../img/hackreactor.png" title="Hack Reactor" alt="" className="img-responsive" /></div>
+        </div>
+        <div className="col-sm-4 col-md-2 col-xs-6">
+          <div className="customer"><img src="../img/hackreactor.png" title="Hack Reactor" alt="" className="img-responsive" /></div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default Customers;
