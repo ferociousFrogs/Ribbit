fe62a190cf7bd81a83569df66e712475f521d1c6
Andy Yeo <andy.yeo@gmail.com>
2017-04-22 15:21:59 -0700

    Link all components as child of Home component

--- client/components/homepage/Home.jsx
+++ client/components/homepage/Home.jsx
@@ -1,29 +1,17 @@
 import React from 'react';
-import CreateRoute from './CreateRoute';
+import Intro from './Intro';
+import Customers from './Customers';
+import Features from './Features';
+import Testimonials from './Testimonials';
+import Contact from './Contact';
 
 const Home = () => (
   <div>
-    <section id="intro" className="text-intro no-padding-bottom">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <h1>Do you code with <span className="rotate">remote team members</span>?</h1>
-            <h3 className="weight-300">Let us help you</h3>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12">
-            <p>500,000 users have already trusted <a href="">Ribbit</a>.
-            <br />
-            Try it for yourself.</p>
-            <CreateRoute />
-          </div>
-        </div>
-        <div className="col-md-12 col-lg-8 col-lg-offset-2">
-          <p className="margin-bottom--zero"><img alt="" src="../../introcode.png" className="img-responsive" /></p>
-        </div>
-      </div>
-    </section>
+    <Intro />
+    <Customers />
+    <Features />
+    <Testimonials />
+    <Contact />
   </div>
 );
 
