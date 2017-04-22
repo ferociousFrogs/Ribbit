831dcede8c7da7346edd60e78a21d0df17406daf
Andy Yeo <andy.yeo@gmail.com>
2017-04-22 15:21:26 -0700

    Add contact component for team

---
+++ client/components/homepage/Contact.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Contact = () => (
+  <section id="contact" className="section background-gray-lightest">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3">
+          <div className="box-simple">
+            <div className="icon" />
+            <h3>Name</h3>
+            <p>Profile Info Summary</p>
+            <p>Github Link</p>
+            <p>LinkedIn Link</p>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="box-simple">
+            <div className="icon" />
+            <h3>Name</h3>
+            <p>Profile Info Summary</p>
+            <p>Github Link</p>
+            <p>LinkedIn Link</p>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="box-simple">
+            <div className="icon" />
+            <h3>Name</h3>
+            <p>Profile Info Summary</p>
+            <p>Github Link</p>
+            <p>LinkedIn Link</p>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="box-simple">
+            <div className="icon" />
+            <h3>Name</h3>
+            <p>Profile Info Summary</p>
+            <p>Github Link</p>
+            <p>LinkedIn Link</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default Contact;
