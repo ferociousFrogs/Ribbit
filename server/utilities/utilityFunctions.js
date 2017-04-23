7bcc909d2d433ef572664be5b3ec8005c50f521a
dshelters <dshelters@gmail.com>
2017-04-22 17:03:27 -0700

    (implement) Create functions to check for or add room and user
    Both functions will return the room or user id, respectively.

--- server/utilities/utilityFunctions.js
+++ server/utilities/utilityFunctions.js
@@ -17,5 +17,36 @@ module.exports = {
     const clientsInRoom = namedRooms[room];
     const numClients = clientsInRoom === undefined ? 0 : Object.keys(clientsInRoom.sockets).length;
     return numClients;
+  },
+
+  checkOrCreateUser: (user) => (
+    // user = {userName, etc.}
+    db.users.findId(user)
+      .then((userId) => {
+        if (typeof userId === 'number') {
+          return userId;
+        }
+        return db.users.add((user));
+      })
+      .catch(err => console.error(`Error checking or creating User ${user.userName} with error = ${err}`))
+  ),
+
+  checkOrCreateRoom: (room) => {
+    // room = {roomName, userId}
+    return db.rooms.findId(room.roomName)
+      .then((roomId) => {
+        if (typeof roomId === 'number') {
+          return roomId;
+        }
+        return db.rooms.add((room.roomName));
+      })
+      .then((roomId) => {
+        if (!room.userId) {
+          return roomId;
+        }
+        room.roomId = roomId;
+        return db.rooms_users.add(room);
+      })
+      .catch(err => console.error(`Error checking or creating Room ${room.roomName} with error = ${err}`));
   }
 };
