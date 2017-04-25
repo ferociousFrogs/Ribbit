ffa76ac808e6c1fd82179a8cd62634e46b1b321d
Andy Yeo <andy.yeo@gmail.com>
2017-04-25 15:49:20 -0700

    Add my profile to contact

--- client/components/homepage/Contact.jsx
+++ client/components/homepage/Contact.jsx
@@ -7,7 +7,17 @@ const Contact = () => (
         <div className="col-md-3">
           <div className="box-simple">
             <div className="icon" />
-            <h3>Name</h3>
+            <h3>Andy Yeo</h3>
+            <p>Profile Info Summary</p>
+            <a href="https://github.com/misteryeo/">github.com/misteryeo/</a>
+            <br />
+            <a href="https://www.linkedin.com/in/andycpyeo/">linkedin.com/in/andycpyeo</a>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="box-simple">
+            <div className="icon" />
+            <h3>Dylan Shelters</h3>
             <p>Profile Info Summary</p>
             <p>Github Link</p>
             <p>LinkedIn Link</p>
@@ -16,7 +26,7 @@ const Contact = () => (
         <div className="col-md-3">
           <div className="box-simple">
             <div className="icon" />
-            <h3>Name</h3>
+            <h3>David Brodie</h3>
             <p>Profile Info Summary</p>
             <p>Github Link</p>
             <p>LinkedIn Link</p>
@@ -25,16 +35,7 @@ const Contact = () => (
         <div className="col-md-3">
           <div className="box-simple">
             <div className="icon" />
-            <h3>Name</h3>
-            <p>Profile Info Summary</p>
-            <p>Github Link</p>
-            <p>LinkedIn Link</p>
-          </div>
-        </div>
-        <div className="col-md-3">
-          <div className="box-simple">
-            <div className="icon" />
-            <h3>Name</h3>
+            <h3>Jeff Millberger</h3>
             <p>Profile Info Summary</p>
             <p>Github Link</p>
             <p>LinkedIn Link</p>
