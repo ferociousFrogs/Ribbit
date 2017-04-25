4237c03a1ea979c4b8c08ea12f30e19aae3001cc
Andy Yeo <andy.yeo@gmail.com>
2017-04-25 15:31:56 -0700

    Update third feature format to align with initial features

--- client/components/homepage/Features.jsx
+++ client/components/homepage/Features.jsx
@@ -11,7 +11,7 @@ const Features = () => (
             <p>Enter description for this feature</p>
           </div>
           <div className="col-md-6">
-            <p><img alt="" src="../img/introcode.png" className="img-responsive" /></p>
+            <p><img alt="" src="../img/collabcode.png" className="img-responsive" /></p>
           </div>
         </div>
       </div>
@@ -20,7 +20,7 @@ const Features = () => (
       <div className="container">
         <div className="row text-center-mobile">
           <div className="col-md-6">
-            <p><img alt="" src="../img/introcode.png" className="img-responsive" /></p>
+            <p><img alt="" src="../img/videochat.png" className="img-responsive" /></p>
           </div>
           <div className="col-md-6">
             <div className="icon brand-terciary"><i className="pe-7s-rocket" /></div>
@@ -30,14 +30,16 @@ const Features = () => (
         </div>
       </div>
     </section>
-    <section className="section no-padding-bottom">
+    <section id="features" className="section">
       <div className="container">
-        <div className="row">
-          <div className="col-md-12 col-lg-8 col-lg-offset-2 text-center">
-            <div className="icon brand-secondary"><i className="pe-7s-graph1" /></div>
+        <div className="row text-center-mobile">
+          <div className="col-md-6">
+            <div className="icon"><i className="pe-7s-diamond" /></div>
             <h2>Text Chat</h2>
             <p>Enter description for this feature</p>
-            <p className="margin-bottom--zero"><img alt="" src="../img/introcode.png" className="img-responsive" /></p>
+          </div>
+          <div className="col-md-6">
+            <p><img alt="" src="../img/textchat.png" className="img-responsive" /></p>
           </div>
         </div>
       </div>
