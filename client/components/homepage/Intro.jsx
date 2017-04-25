2a3bfa931f5ac02a3fbeb3f943abdd3a26dfb170
Andy Yeo <andy.yeo@gmail.com>
2017-04-25 13:46:03 -0700

    Modify copy for intro

--- client/components/homepage/Intro.jsx
+++ client/components/homepage/Intro.jsx
@@ -6,15 +6,13 @@ const Intro = () => (
     <div className="container">
       <div className="row">
         <div className="col-md-12">
-          <h1>Do you code with <span className="rotate">remote team members</span>?</h1>
-          <h3 className="weight-300">Let us help you</h3>
+          <h1>Save time <span className="rotate">collaborating</span> with your team</h1>
+          <h3 className="weight-300">Code and communicate all in one place</h3>
         </div>
       </div>
       <div className="row">
         <div className="col-md-12">
-          <p>500,000 users have already trusted <a href="">Ribbit</a>.
-          <br />
-          Try it for yourself.</p>
+          <p>Get started now.</p>
           <CreateRoute />
         </div>
       </div>
