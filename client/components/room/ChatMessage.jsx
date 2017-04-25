167a43121576b99861f32b95c8b6162c18d56062
Andy <andy.yeo@gmail.com>
2017-04-24 22:49:56 -0700

    Updated ChatMessage.jsx
    Update the data issue

--- client/components/room/ChatMessage.jsx
+++ client/components/room/ChatMessage.jsx
@@ -8,7 +8,7 @@ const ChatMessage = props => (
           <img src="http://placehold.it/50/00A5D2/fff&text=ME" alt="User Avatar" className="img-circle" />
         </span>
         <div className="chat-name-from-me"><strong>{props.message.userName}</strong></div>
-        <div className="chat-text-from-me">{props.message.text}</div>
+        <div className="chat-text-from-me">{props.message.data}</div>
       </div>
     :
       <div className="chat-message-container-other">
@@ -16,7 +16,7 @@ const ChatMessage = props => (
           <img src="http://placehold.it/50/FF7473/fff&text=YOU" alt="User Avatar" className="img-circle" />
         </span>
         <div className="chat-name-from-other"><strong>{props.message.userName}</strong></div>
-        <div className="chat-text-from-other">{props.message.text}</div>
+        <div className="chat-text-from-other">{props.message.data}</div>
       </div>
     }
   </div>
