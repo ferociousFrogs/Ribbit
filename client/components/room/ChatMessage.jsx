ef6b27647c525f056117aefbfe2da3e2b3b4c6f3
Andy Yeo <andy.yeo@gmail.com>
2017-04-24 18:22:02 -0700

    Style chat messages and window

--- client/components/room/ChatMessage.jsx
+++ client/components/room/ChatMessage.jsx
@@ -4,13 +4,19 @@ const ChatMessage = props => (
   <div>
     { props.message.fromMe ?
       <div className="chat-message-container-me">
-          <div className="chat-name-from-me chat-message-name"><strong>{props.message.userName}</strong></div>
-          <div className="chat-text-from-me">{props.message.data}</div>
+        <span className="pull-left">
+          <img src="http://placehold.it/50/00A5D2/fff&text=ME" alt="User Avatar" className="img-circle" />
+        </span>
+        <div className="chat-name-from-me"><strong>{props.message.userName}</strong></div>
+        <div className="chat-text-from-me">{props.message.text}</div>
       </div>
     :
       <div className="chat-message-container-other">
-        <div className="chat-message-name"><strong>{`Guest ${props.message.userName}`}</strong></div>
-        <div className="chat-text-from-other">{props.message.data}</div>
+        <span className="pull-right">
+          <img src="http://placehold.it/50/FF7473/fff&text=YOU" alt="User Avatar" className="img-circle" />
+        </span>
+        <div className="chat-name-from-other"><strong>{props.message.userName}</strong></div>
+        <div className="chat-text-from-other">{props.message.text}</div>
       </div>
     }
   </div>
