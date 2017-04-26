fa4576268049176f9abd98bd0a04b4abfa316a2f
David Brodie <david.brodie122@gmail.com>
2017-04-25 20:23:18 -0700

    (feat)Update functions in Room.jsx.  Satisfy linter

--- client/components/profile/ProfilePartnersListItem.jsx
+++ client/components/profile/ProfilePartnersListItem.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 // []each partner needs to be clickable
 // []onClick, each partner needs to send out a function that will grab the code and messages connected to that room and connected to the logged in user/
 // []this will populate a space in state - partnerLogs
-  // []we need an action - getPartnerLogs which takes in an object that has two keys {code: '', messages: []}
+  // [X]we need an action - getPartnerLogs which takes in an object that has two keys {code: '', messages: []}
   // []messages need to be sorted by timestamp, with oldest being placed in the array in state first
-  // []we need a reducer - partnerLogsReducer
+  // [X]we need a reducer - partnerLogsReducer
 const ProfilePartnersListItem = props => (
   <div>
     {props.partner.name}
