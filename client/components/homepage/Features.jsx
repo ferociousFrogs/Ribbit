7999c09206d4890f148052686ca3412dcbbf4cd2
dshelters <dshelters@gmail.com>
2017-04-27 21:47:54 -0700

    (fix) Refine copy on features page.

--- client/components/homepage/Features.jsx
+++ client/components/homepage/Features.jsx
@@ -10,6 +10,7 @@ const Features = () => (
             <h2>Real-time Collaborative Coding</h2>
             <p>Instantly share code with your peers.</p>
             <p>Work together to solve problems, even running code right in your browser.</p>
+            <p>Even drag and drop text into the coding environment to start coding from a file you have in progress.</p>
           </div>
           <div className="col-md-6">
             <p><img alt="" src="../img/collabcode.png" className="img-responsive" /></p>
@@ -39,7 +40,7 @@ const Features = () => (
             <div className="icon"><i className="glyphicon glyphicon-comment" /></div>
             <h2>Text Chat</h2>
             <p>Somethings just need to be written down.</p>
-            <p>Messages your colleagues while coding to send important information and access it anytime by signing up.</p>
+            <p>Message your colleagues while coding to send important information and access it anytime by signing up.</p>
           </div>
           <div className="col-md-6">
             <p><img alt="" src="../img/textchat.png" className="img-responsive" /></p>
