5e5d0bc16b72c813826e67dba2ded18b07f67665
Andy Yeo <andy.yeo@gmail.com>
2017-04-27 21:30:06 -0700

    Add test set up files for Jest for react-slick

--- spec/client-spec/App.test.jsx
+++ spec/client-spec/App.test.jsx
@@ -4,9 +4,19 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from '../../client/components/App';
 import { Provider } from 'react-redux';
 import store from './../../client/store';
+import Slider from 'react-slick';
+
 
 test('App component snapshot test', () => {
   const component = renderer.create(<Provider store={store}><Router><App /></Router></Provider>);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+window.matchMedia = window.matchMedia || function() {
+    return {
+        matches : false,
+        addListener : function() {},
+        removeListener: function() {}
+    };
+};
