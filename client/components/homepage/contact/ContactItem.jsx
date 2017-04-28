8bfae25a2367d8874ac1519e486fc006f6256069
dshelters <dshelters@gmail.com>
2017-04-28 11:11:35 -0700

    (style) Increase the image size for the contact info

--- client/components/homepage/contact/ContactItem.jsx
+++ client/components/homepage/contact/ContactItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ContactItem = props => (
   <div className="col-xs-12 col-sm-6 col-md-3 text-center">
     <div className="box-simple">
-      <img src={`../../img/${props.name}.jpg`} className="icon" />
+      <img src={`../../img/${props.name}.jpg`} className="about-img" />
       <h3>{props.name}</h3>
       <p>Profile Info Summary</p>
       <a href={`https://${props.github}`} >{props.github}</a>
